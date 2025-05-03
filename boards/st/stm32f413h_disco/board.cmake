7fcd70a86628ccb521229372c769dd13db276801
Samuel Quiniou <samuel.quiniou@rtone.fr>
2025-05-03 23:18:11 +0200

    boards: stm32f413h_disco: add pyocd support
    Enable pyOCD support for the STM32F413H-DISCO board.
    
    Signed-off-by: Samuel Quiniou samuel.quiniou@rtone.fr

--- boards/st/stm32f413h_disco/board.cmake
+++ boards/st/stm32f413h_disco/board.cmake
@@ -3,8 +3,11 @@
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32F413ZH" "--speed=4000")
+board_runner_args(pyocd "--target=stm32f413zh")
+board_runner_args(pyocd "--flash-opt=-O reset_type=hw")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
